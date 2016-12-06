61e1662027cb99d0d1b862c6fc1b7f71078fdc86
Pascal Jacquemart <P99_Pascal@yahoo.fr>
2016-12-07 08:50:29 +1100

    Initial Xcode 8.1 project

---
+++ Pacman/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Pacman
+//
+//  Created by Palou & Audrey J on 07/12/2016.
+//  Copyright © 2016 Pascal Jacquemart. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
