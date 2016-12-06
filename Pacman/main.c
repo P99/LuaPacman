8d5ef346210113d351d7d07abd71dbe07d61c150
Pascal Jacquemart <P99_Pascal@yahoo.fr>
2016-12-07 08:55:03 +1100

    Adding basic main creating SDL window

--- Pacman/main.c
+++ Pacman/main.c
@@ -6,10 +6,44 @@
 //  Copyright © 2016 Pascal Jacquemart. All rights reserved.
 //
 
+#include <SDL2/SDL.h>
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main( int argc, char* args[] )
+{
+    SDL_Window* window = NULL;
+    
+    SDL_Surface* screenSurface = NULL;
+    
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    {
+        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+    }
+    else
+    {
+        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        if( window == NULL )
+        {
+            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+        }
+        else
+        {
+            screenSurface = SDL_GetWindowSurface( window );
+            
+            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0x00, 0xFF, 0x00 ) );
+            
+            SDL_UpdateWindowSurface( window );
+
+            SDL_Delay( 5000 );
+        }
+    }
+
+    SDL_DestroyWindow( window );
+    
+    SDL_Quit();
+    
     return 0;
 }
