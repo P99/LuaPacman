0e2558708777091b92c0b0d18f8eab7458fb71d2
Pascal Jacquemart <P99_Pascal@yahoo.fr>
2016-12-07 14:04:01 +1100

    moving to OOP

--- Pacman/pacman.lua
+++ Pacman/pacman.lua
@@ -1,25 +1,47 @@
-print("Pacman game in Lua")
+
+-- Pacman game in Lua OOP
+Game = {}
 
 -- Load map file and draw the whole thing
-function load_map(filename)
+function Game:loadMap(filename)
+  local actions = {
+    ["#"] = function () print("Create wall") end,
+    ["@"] = function () print("Create coin") end,
+  }
   local f = assert(io.open(filename, "r"))
   while true do
     local line = f:read("*line")
     if line == nil then break end
-    --print("line: " .. line)
+    for i = 1, #line do
+      local type = line:sub(i, i)
+      if actions[type] ~= nill then
+        actions[type]()
+      end
+    end
   end
   f:close()
 end
 
-load_map("map.txt")
+--drawRect{top=5, left=24, width=53, height=79}
+--drawRect{top=15, left=54, width=5, height=79}
+--drawRect{top=45, left=24, width=3, height=75}
 
-drawRect{top=5, left=24, width=53, height=79}
-drawRect{top=15, left=54, width=5, height=79}
-drawRect{top=45, left=24, width=3, height=75}
 
-while true do
-  local events = getKey()
-  if events ~= nil then
-    print("Event: " .. events)
+function Game:eventHandler()
+  while true do
+    local events = getKey()
+    if events ~= nil then
+      print("Event: " .. events)
+    end
   end
 end
+
+function Game:init()
+  print("Pacman game in Lua")
+  Game:loadMap("map.txt")
+  Game:eventHandler()
+end
+
+Game:init()
+
+
