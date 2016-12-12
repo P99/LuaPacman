8c66a6053262fe4dad09782bab5a5eff6999e182
Pascal Jacquemart <P99_Pascal@yahoo.fr>
2016-12-12 16:31:18 +1100

    Update Pacman sprites + animation stages

--- Pacman/character.lua
+++ Pacman/character.lua
@@ -7,12 +7,29 @@ function Character:init()
   setmetatable(o, self)
   self.__index = self
   o.position = { x = 0, y = 0 }
-  o.direction = { x = 0, y = 0 }
+  o.direction = ""
   o.rect = {}
+  o.animation_stage = 1
   return o;
 end
 
 function Character:setPosition(x, y)
+  if self.position.x < x then
+    self.direction = "Right"
+  elseif self.position.x > x then
+    self.direction = "Left"
+  end
+
+  if self.position.y < y then
+    self.direction = "Down"
+  elseif self.position.y > y then
+    self.direction = "Up"
+  end
+
+  self.animation_stage = ((self.animation_stage + 1) % 3) +1
+
+  --print("Character direction: " .. self.direction);
+
   self.position.x = x
   self.position.y = y
   --print("SetPosition " .. x .. " " .. y)
@@ -23,16 +40,30 @@ function Character:setRect(rect)
 end
 
 function Character:draw()
-  sprite = {
-    src_top = 0,
-    src_left = 0,
-    src_width = 22,
-    src_height = 22,
-    dst_top = self.rect.top,
-    dst_left = self.rect.left,
-    dst_width = self.rect.width,
-    dst_height = self.rect.height
+  local sprite_metadata = {
+    Left = {
+      {0, 0, 22, 22}, { 22, 0, 22, 22 }, {44, 0, 22, 22}
+    },
+    Right = {
+      {0, 22, 22, 22}, { 22, 22, 22, 22 }, {44, 0, 22, 22}
+    },
+    Up = {
+      {0, 44, 22, 22}, { 22, 44, 22, 22 }, {44, 0, 22, 22}
+    },
+    Down = {
+      {0, 66, 22, 22}, { 22, 66, 22, 22 }, {44, 0, 22, 22}
+    }
   }
+  sprite = {}
+  sprite.src_left = sprite_metadata[self.direction][self.animation_stage][1]
+  sprite.src_top = sprite_metadata[self.direction][self.animation_stage][2]
+  sprite.src_width = sprite_metadata[self.direction][self.animation_stage][3]
+  sprite.src_height = sprite_metadata[self.direction][self.animation_stage][4]
+  sprite.dst_top = self.rect.top
+  sprite.dst_left = self.rect.left
+  sprite.dst_width = self.rect.width
+  sprite.dst_height = self.rect.height
+
   drawSprite(sprite)
 end
 
